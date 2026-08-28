042076d3c42125bf465379168241e01c58a692d5
Martim Andersen <martimandersen@gmail.com>
2026-08-28 15:49:13 +0100

    Add spring init backend

---
+++ backend/src/main/java/com/martim/adventure_book/AdventureBookApplication.java
@@ -0,0 +1,13 @@
+package com.martim.adventure_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdventureBookApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/martim/adventure_book/AdventureBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martim.adventure_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
