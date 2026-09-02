9a5b4fa473f5af41adc363988c6a6ed79d8e66d1
Martim Andersen <martim.andersen@koerber.com>
2026-09-02 23:33:29 +0100

    Add list books with angular material; Add john-plays-football book

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -2,10 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 import { BooksService } from '../../core/services/books.service';
 import { BookSummary } from '../../core/models/book-summary';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
