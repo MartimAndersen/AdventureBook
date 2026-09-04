40215f8a0ded580a484c9360dc7302d88a778daf
Martim Andersen <martim.andersen@koerber.com>
2026-09-04 20:33:12 +0100

    Add game over/success messages

--- frontend/src/app/core/models/game-status.ts
+++ frontend/src/app/core/models/game-status.ts
@@ -1,5 +1,4 @@
 export enum GameStatus {
-  IN_PROGRESS = 'IN_PROGRESS',
   WON = 'WON',
   LOST = 'LOST',
 }
--- frontend/src/app/pages/game/game.ts
+++ frontend/src/app/pages/game/game.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { GamesService } from '../../core/services/games.service';
 import { Game as GameModel } from '../../core/models/game';
+import { GameStatus } from '../../core/models/game-status';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -13,6 +14,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class Game implements OnInit {
   game = signal<GameModel | undefined>(undefined);
+  readonly gameStatus = GameStatus;
 
   constructor(
     private gamesService: GamesService,
