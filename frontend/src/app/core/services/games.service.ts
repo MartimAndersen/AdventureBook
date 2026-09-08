dfc2c591d0540a767eb41db8ac65aadb299c87c7
Martim Andersen <martim.andersen@koerber.com>
2026-09-08 18:47:09 +0100

    Add explicit returns on GamesService frontend

--- frontend/src/app/core/services/games.service.ts
+++ frontend/src/app/core/services/games.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { SavedGame } from '../models/saved-game';
 import { Game } from '../models/game';
 
@@ -11,23 +12,23 @@ export class GamesService {
 
   constructor(private http: HttpClient) {}
 
-  getSavedGames() {
+  getSavedGames(): Observable<SavedGame[]> {
     return this.http.get<SavedGame[]>(`${this.apiUrl}/saved-games`);
   }
 
-  startGame(bookId: string) {
+  startGame(bookId: string): Observable<Game> {
     return this.http.post<Game>(`${this.apiUrl}/games`, { bookId });
   }
 
-  getGame(gameId: string) {
+  getGame(gameId: string): Observable<Game> {
     return this.http.get<Game>(`${this.apiUrl}/games/${gameId}`);
   }
 
-  makeChoice(gameId: string, optionIndex: number) {
+  makeChoice(gameId: string, optionIndex: number): Observable<Game> {
     return this.http.post<Game>(`${this.apiUrl}/games/${gameId}/choices`, { optionIndex });
   }
 
-  saveGame(gameId: string) {
+  saveGame(gameId: string): Observable<void> {
     return this.http.post<void>(`${this.apiUrl}/games/${gameId}/save`, null);
   }
 }
